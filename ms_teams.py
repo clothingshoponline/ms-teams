0348e4d4b1aa18ac757649a32625fb64fa8d6274
Nazneen Peracha <56941470+nazneenperacha@users.noreply.github.com>
2020-09-03 16:04:46 -0700

    Make function use more clear - Change send_msteams to send_text - Change channel to webhook

--- ms_teams.py
+++ ms_teams.py
@@ -3,11 +3,11 @@ import pymsteams
 
 
 
-def send_msteams(message: str, program_name: str, channel: str = MSTEAMS_TEST, 
+def send_text(message: str, program_name: str, webhook: str, 
                  include_traceback: bool = False):
     ''' Send the message to the specified Microsoft Teams channel. 
     If include_traceback is True, send the most recent exception raised.'''
-    ms_message = pymsteams.connectorcard(channel)
+    ms_message = pymsteams.connectorcard(webhook)
     ms_message.title(program_name)
     if include_traceback:
         message = f'{message}\n{traceback.format_exc()}'
