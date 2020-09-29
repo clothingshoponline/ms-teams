a56c2b7364cf7fdfe4b218f588be92fa66d737ca
Nazneen Peracha <56941470+nazneenperacha@users.noreply.github.com>
2020-09-29 15:05:04 -0700

    Create class that represents a Teams channel

--- ms_teams.py
+++ ms_teams.py
@@ -9,3 +9,14 @@ def send_text(message: str, title: str, webhook: str) -> None:
     ms_message.text(f'<pre>\n{message}\n</pre>')
     
     ms_message.send()
+
+
+
+
+class Channel:
+    def __init__(self, team_id: str, channel_id: str, 
+                 bearer: str):
+        """Initialize a Channel."""
+        self._team_id: team_id
+        self._channel_id = channel_id
+        self._bearer = bearer
