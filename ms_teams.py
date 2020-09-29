d5fc188d4c03d847f3148c1885ac6a6ca034a7bb
Nazneen Peracha <56941470+nazneenperacha@users.noreply.github.com>
2020-09-29 11:13:20 -0700

    Delete send_text ability to send traceback

--- ms_teams.py
+++ ms_teams.py
@@ -2,15 +2,10 @@ import pymsteams
 
 
 
-
-def send_text(message: str, program_name: str, webhook: str, 
-                 include_traceback: bool = False):
-    ''' Send the message to the specified Microsoft Teams channel. 
-    If include_traceback is True, send the most recent exception raised.'''
+def send_text(message: str, title: str, webhook: str) -> None:
+    """Send the message to the specified Microsoft Teams channel."""
     ms_message = pymsteams.connectorcard(webhook)
-    ms_message.title(program_name)
-    if include_traceback:
-        message = f'{message}\n{traceback.format_exc()}'
+    ms_message.title(title)
     ms_message.text(f'<pre>\n{message}\n</pre>')
     
     ms_message.send()
